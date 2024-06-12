58bdc69221bed3ceae6611570f357ee86ff2f57b
Max <52867741+SpaceYoshi@users.noreply.github.com>
2024-06-12 17:08:08 +0200

    Create Exercise1-4.cpp

---
+++ ComputerVision/Exercise1-4.cpp
@@ -0,0 +1,29 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void main() {
+	string path = "Resources/sao-p-manga.png";
+	Mat img = imread(path);
+	Mat matrix, imgWarp;
+	float w = 500, h = 700;
+
+	Point2f src[4] = { {463,358},{729,491},{39,540},{304,822} };
+	Point2f dst[4] = { {0.0f,0.0f},{w,0.0f},{0.0f,h},{w,h} };
+
+	matrix = getPerspectiveTransform(src, dst);
+	warpPerspective(img, imgWarp, matrix, Point(w, h));
+
+	for (int i = 0; i < 4; i++)
+	{
+		circle(img, src[i], 10, Scalar(0, 0, 255), FILLED);
+	}
+
+	imshow("Image", img);
+	imshow("Image Warp", imgWarp);
+	waitKey(0);
+}
